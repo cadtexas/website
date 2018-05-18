da29d140167ae19fa1f235da004d64d0c9d594a1
Miguel Jose Obregon <miguel.j.obregon@gmail.com>
2018-05-18 12:34:29 -0500

    Update officers for Fall 2018

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,24 @@ var router = express.Router();
 
 var officers = [
   {
-    name: 'Kishan Ramchandran',
-    title: 'President',
-    image: 'images/officers/kishan-ramchandran.jpg'
-  },
-  {
-    name: 'Alvin Nguyen',
-    title: 'Vice President',
-    image: 'images/officers/alvin-nguyen.jpg'
-  },
-  {
     name: 'Carl Karouta',
-    title: 'Corporate Director',
+    title: 'President',
     image: 'images/officers/carl-karouta.jpg'
   },
   {
     name: 'Alex Collins',
-    title: 'Events Director',
+    title: 'VP Internal',
     image: 'images/officers/alex-collins.png'
+  },
+  {
+    name: 'Dan Tran',
+    title: 'VP External',
+    image: 'images/officers/dan-tran.png'
+  },
+  {
+    name: 'Miguel Obregon',
+    title: 'VP Special Events',
+    image: 'images/officers/miguel-obregon.jpg'
   },
   {
     name: 'Karina Galvan',
@@ -28,44 +28,14 @@ var officers = [
     image: 'images/officers/karina-galvan.jpg'
   },
   {
-    name: 'Steven Kornajcik',
-    title: 'Membership Director',
-    image: 'images/officers/steven-kornajcik.png'
-  },
-  {
-    name: 'Dan Tran',
-    title: 'Treasurer',
-    image: 'images/officers/dan-tran.png'
-  },
-  {
-    name: 'Shaunik Bhatte',
-    title: 'SEC Representative',
-    image: 'images/officers/shaunik-bhatte.png'
-  },
-  {
-    name: 'Aaron Comen',
-    title: 'Instructor',
-    image: 'images/officers/aaron-comen.png'
-  },
-  {
     name: 'David Garza',
     title: 'Instructor',
     image: 'images/officers/david-garza.jpg'
   },
   {
-    name: 'Eduardo Priego',
-    title: 'Instructor',
-    image: 'images/officers/eduardo-priego.png'
-  },
-  {
     name: 'Juan Trejo',
     title: 'Instructor',
     image: 'images/officers/juan-trejo.jpg'
-  },
-  {
-    name: 'Miguel Obregon',
-    title: 'Instructor',
-    image: 'images/officers/miguel-obregon.jpg'
   },
 ];
 
