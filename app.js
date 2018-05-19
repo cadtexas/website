89c80e79ab32688f7321e5a5b39aa72bca8ae2c6
Miguel Jose Obregon <miguel.j.obregon@gmail.com>
2018-05-19 09:09:43 -0500

    Remove less files and middleware

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var expressLess = require('express-less');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -22,7 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', expressLess(path.join(__dirname, 'less'), { debug: true }));
 
 app.use('/', index);
 app.use('/workshop', workshop);
