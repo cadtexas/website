3ec05f12723ef731bb42380e039c976463adc915
Miguel Jose Obregon <miguel.j.obregon@gmail.com>
2018-05-19 09:39:32 -0500

    Add officers page

--- routes/index.js
+++ routes/index.js
@@ -5,43 +5,55 @@ var officers = [
   {
     name: 'Carl Karouta',
     title: 'President',
-    image: 'images/officers/carl-karouta.jpg'
+    image: 'images/officers/carl-karouta.jpg',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus erat quis."
   },
   {
     name: 'Alex Collins',
     title: 'VP Internal',
-    image: 'images/officers/alex-collins.png'
+    image: 'images/officers/alex-collins.png',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus erat quis."
   },
   {
     name: 'Dan Tran',
     title: 'VP External',
-    image: 'images/officers/dan-tran.png'
+    image: 'images/officers/dan-tran.png',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus erat quis."
   },
   {
     name: 'Miguel Obregon',
     title: 'VP Special Events',
-    image: 'images/officers/miguel-obregon.jpg'
+    image: 'images/officers/miguel-obregon.jpg',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus erat quis."
   },
   {
     name: 'Karina Galvan',
     title: 'Media Director',
-    image: 'images/officers/karina-galvan.jpg'
+    image: 'images/officers/karina-galvan.jpg',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus erat quis."
   },
   {
     name: 'David Garza',
     title: 'Instructor',
-    image: 'images/officers/david-garza.jpg'
+    image: 'images/officers/david-garza.jpg',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus erat quis."
   },
   {
     name: 'Juan Trejo',
     title: 'Instructor',
-    image: 'images/officers/juan-trejo.jpg'
+    image: 'images/officers/juan-trejo.jpg',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus erat quis."
   },
 ];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'UT CAD' });
+});
+
+/* GET officers page */
+router.get('/officers', function(req, res, next) {
+  res.render('officers', { title: 'CAD - Officers', 'officers': officers });
 });
 
 module.exports = router;
